033d545503b51e1855c982bebfc40064fe1a03f2
bgavran3 <bruno.gavranovic@fer.hr>
2017-11-15 15:03:57 +0100

    Synth grads work now with more layers

--- src/decoupled_mnist.py
+++ src/decoupled_mnist.py
@@ -11,7 +11,8 @@ gpu = True
 max_epochs = 10
 
 input_size = 784
-hidden_size = 32
+hidden_size_1 = 32
+hidden_size_2 = 32
 out_size = 10
 
 batch_size = 128
@@ -22,23 +23,31 @@ mnist = MNIST(batch_size)
 squared_loss_sum = SquaredDifferenceLoss()
 ce_loss = nn.CrossEntropyLoss()
 
-module1 = OneLayer(input_size, hidden_size)
+module1 = OneLayer(input_size, hidden_size_1)
 module1_opt = torch.optim.Adam(module1.parameters(), lr=lr)
 
-module2 = OneLayer(hidden_size, out_size)
+module2 = OneLayer(hidden_size_1, hidden_size_2)
 module2_opt = torch.optim.Adam(module2.parameters(), lr=lr)
 
-grad_module1 = GradModule(hidden_size, module1.w.data.shape)
+module3 = OneLayer(hidden_size_2, out_size)
+module3_opt = torch.optim.Adam(module3.parameters(), lr=lr)
+
+grad_module1 = GradModule(hidden_size_1, module1.w.data.shape)
 grad_module1_opt = torch.optim.Adam(grad_module1.parameters(), lr=lr)
+
+grad_module2 = GradModule(hidden_size_2, module2.w.data.shape)
+grad_module2_opt = torch.optim.Adam(grad_module2.parameters(), lr=lr)
 
 if gpu:
     module1 = module1.cuda()
     module2 = module2.cuda()
+    module3 = module3.cuda()
     grad_module1 = grad_module1.cuda()
+    grad_module2 = grad_module2.cuda()
 
 
 def network_output(images):
-    return module2(module1(images))
+    return module3(module2(module1(images)))
 
 
 def test_network():
@@ -54,7 +63,7 @@ def test_network():
         total += labels.size(0)  # add amount in one batch
         correct += (predicted == labels).sum()
 
-    print("Accuracy on 10000 test images:", 100 * correct / total)
+    print("Accuracy on 10000 test images:", 100 * correct / total, "\n\n")
 
 
 def print_fn(step):
@@ -68,22 +77,40 @@ for epoch in range(max_epochs):
         images = Variable(images.view(-1, 28 * 28))
         labels = Variable(labels)
 
+        # Training first layer with synthetic gradients
         module1_opt.zero_grad()
         module1_output = module1(images)
         mod1_generated_grad = grad_module1(module1_output)
         module1.set_grads(mod1_generated_grad)
         module1_opt.step()
 
-        # Training last (second) layer
+        # Training second layer with synthetic gradients
         module2_opt.zero_grad()
         module2_output = module2(module1(images))
-        mod2_cost = ce_loss(module2_output, labels)
-        mod2_cost.backward(retain_graph=True)
+        mod2_generated_grad = grad_module2(module2_output)
+        module2.set_grads(mod2_generated_grad)
         module2_opt.step()
 
-        # Training the gradient module
+        # Training third (last) layer with real gradients
+        module3_opt.zero_grad()
+        module3_output = module3(module2(module1(images)))
+        mod3_cost = ce_loss(module3_output, labels)
+        mod3_cost.backward(retain_graph=True)
+        module3_opt.step()
+
+        # Training gradient module 2
+        grad_module2_opt.zero_grad()
+        mod2_true_grad = torch.autograd.grad(mod3_cost, module2.w,
+                                             retain_graph=True)[0]
+        mod2_true_grad.volatile = False  # why do I have to set this?
+        grad_mod2_cost = squared_loss_sum(mod2_true_grad, mod2_generated_grad)
+        grad_mod2_cost.backward(retain_graph=True)
+        grad_module2_opt.step()
+
+        # Training gradient module 1
         grad_module1_opt.zero_grad()
-        mod1_true_grad = torch.autograd.grad(mod2_cost, module1.w, retain_graph=True)[0]
+        mod1_true_grad = torch.autograd.grad(mod3_cost, module1.w,
+                                             retain_graph=True)[0]
         mod1_true_grad.volatile = False  # why do I have to set this?
         grad_mod1_cost = squared_loss_sum(mod1_true_grad, mod1_generated_grad)
         grad_mod1_cost.backward()
@@ -92,10 +119,16 @@ for epoch in range(max_epochs):
         if print_fn(step):
             try:
                 print("epoch", epoch, "   step", step)
-                text = "mod2_cost {:.2f}, grad_mod1_cost {:.2f}, mod1_generated_grads norm {:.2f}"
-                print(text.format(float(mod2_cost.data.cpu().numpy()),
-                                  float(grad_mod1_cost.data.cpu().numpy()),
-                                  float(mod1_generated_grad.norm().cpu().data.numpy())))
+                text = "grad_mod1_cost {:.2f}, " \
+                       "grad_mod2_cost {:.2f}, " \
+                       "mod3_cost {:.2f}, " \
+                       "mod1_generated_grads norm {:.2f}, " \
+                       "mod2_generated_grads norm {:.2f} "
+                print(text.format(float(grad_mod1_cost.data.cpu().numpy()),
+                                  float(grad_mod2_cost.data.cpu().numpy()),
+                                  float(mod3_cost.data.cpu().numpy()),
+                                  float(mod1_generated_grad.norm().cpu().data.numpy()),
+                                  float(mod2_generated_grad.norm().cpu().data.numpy())))
             except NameError:
                 pass
             test_network()
