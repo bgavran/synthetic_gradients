6013548588aa57c07bf7b57351537dc39f94e70e
bgavran3 <bruno.gavranovic@fer.hr>
2017-11-17 15:50:28 +0100

    Added input module, refactored

--- src/decoupled_mnist.py
+++ src/decoupled_mnist.py
@@ -4,67 +4,64 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 from src.data_loader import MNIST
-from src.modules import OneLayer, GradModule, SquaredDifferenceLoss
+from src.modules import Affine, Module
 from src.plot import *
 
 gpu = True
 
-max_epochs = 2
+max_epochs = 10
 
 input_size = 784
 hidden_size_1 = 128
 hidden_size_2 = 128
 out_size = 10
 
-batch_size = 128
+batch_size = 64
 lr = 0.001
 
 mnist = MNIST(batch_size)
 
-squared_loss_sum = SquaredDifferenceLoss()
-
-
-def optimize_module(module_optimizer, module_generated_sample, module_true_sample):
-    module_optimizer.zero_grad()
-    module_true_sample.volatile = False  # why do I have to set this?
-    module_cost = squared_loss_sum(module_true_sample, module_generated_sample)
-    module_cost.backward(retain_graph=True)
-    module_optimizer.step()
-    return module_cost
-
-
-module1 = OneLayer(input_size, hidden_size_1, nonlinearity=nn.ReLU)
-module1_opt = torch.optim.Adam(module1.parameters(), lr=lr)
+########
+layer1 = Affine(input_size, hidden_size_1, nonlinearity=nn.ReLU)
+layer1_opt = torch.optim.Adam(layer1.parameters(), lr=lr)
 plot_mod1_gen_grad_norm = Plot("Module 1 norm of generated gradients")
 
-grad_module1 = GradModule(hidden_size_1, module1.w.data.shape)
-grad_module1_opt = torch.optim.Adam(grad_module1.parameters(), lr=lr)
-plot_grad_mod1_loss = Plot("Gradient Module 1 loss")
+grad_module1 = Module(hidden_size_1, hidden_size_1, lr=lr, name="Gradient Module 1", reshape_and_sum=False)
 
-module2 = OneLayer(hidden_size_1, hidden_size_2, nonlinearity=nn.ReLU)
-module2_opt = torch.optim.Adam(module2.parameters(), lr=lr)
+########
+layer2 = Affine(hidden_size_1, hidden_size_2, nonlinearity=nn.ReLU)
+layer2_opt = torch.optim.Adam(layer2.parameters(), lr=lr)
 plot_mod2_gen_grad_norm = Plot("Module 2 norm of generated gradients")
 
-grad_module2 = GradModule(hidden_size_2, module2.w.data.shape)
-grad_module2_opt = torch.optim.Adam(grad_module2.parameters(), lr=lr)
-plot_grad_mod2_loss = Plot("Gradient Module 2 loss")
+grad_module2 = Module(hidden_size_2, hidden_size_2, lr=lr, name="Gradient Module 2", reshape_and_sum=False)
 
-module3 = OneLayer(hidden_size_2, out_size)
-module3_opt = torch.optim.Adam(module3.parameters(), lr=lr)
+########
+layer3 = Affine(hidden_size_2, out_size)
+layer3_opt = torch.optim.Adam(layer3.parameters(), lr=lr)
+
+inp_module_3 = Module(input_size,
+                      hidden_size_2,
+                      zero_init=False,
+                      reshape_and_sum=False,
+                      name="Input Module 3")
+
 plot_mod3_loss = Plot("Module 3 loss")
 
 ce_loss = nn.CrossEntropyLoss()
 
+########
+
 if gpu:
-    module1 = module1.cuda()
-    module2 = module2.cuda()
-    module3 = module3.cuda()
+    layer1 = layer1.cuda()
+    layer2 = layer2.cuda()
+    layer3 = layer3.cuda()
     grad_module1 = grad_module1.cuda()
     grad_module2 = grad_module2.cuda()
+    inp_module_3 = inp_module_3.cuda()
 
 
 def network_output(images):
-    return module3(module2(module1(images)))
+    return layer3(layer2(layer1(images)))
 
 
 def test_network():
@@ -83,10 +80,6 @@ def test_network():
     print("Accuracy on 10000 test images:", 100 * correct / total, "\n\n")
 
 
-def print_fn(step):
-    return step % 100 == 0
-
-
 print("Initialized.")
 for epoch in range(max_epochs):
     for step, (images, labels) in enumerate(mnist.train_loader):
@@ -96,63 +89,71 @@ for epoch in range(max_epochs):
         labels = Variable(labels)
 
         # Training first layer with synthetic gradients
-        module1_opt.zero_grad()
-        module1_output = module1(images)
-        mod1_generated_grad = grad_module1(module1_output)
-        module1.set_grads(mod1_generated_grad)
-        module1_opt.step()
+        layer1_opt.zero_grad()
+        layer1_output = layer1(images)
+        layer1_generated_grad = grad_module1(layer1_output)
+        layer1.set_grads(layer1_output, layer1_generated_grad)
+        layer1_opt.step()
 
         # Training second layer with synthetic gradients
-        module2_opt.zero_grad()
-        module2_output = module2(module1_output)
-        mod2_generated_grad = grad_module2(module2_output)
-        module2.set_grads(mod2_generated_grad)
-        module2_opt.step()
+        layer2_opt.zero_grad()
+        layer2_output = layer2(layer1_output)
+        layer2_generated_grad = grad_module2(layer2_output)
+        layer2.set_grads(layer2_output, layer2_generated_grad)
+        layer2_opt.step()
 
-        # Training third (last) layer with real gradients
-        module3_opt.zero_grad()
-        module3_output = module3(module2_output)
-        mod3_cost = ce_loss(module3_output, labels)
+        # Training third (last) layer with synthetic input and real gradients
+        layer3_opt.zero_grad()
+        inp_mod3_generated_sample = inp_module_3(images)
+        layer3_output = layer3(inp_mod3_generated_sample)
+        mod3_cost = ce_loss(layer3_output, labels)
         mod3_cost.backward(retain_graph=True)
-        module3_opt.step()
+        layer3_opt.step()
 
-        mod2_output_true_grad, mod2_w_true_grad = torch.autograd.grad(mod3_cost,
-                                                                      [module2_output] + list(module2.parameters()),
-                                                                      grad_outputs=None,
-                                                                      retain_graph=True)
-        grad_mod2_cost = optimize_module(grad_module2_opt,
-                                         mod2_generated_grad,
-                                         mod2_w_true_grad)
+        ###
+        grad_mod1_cost = Variable(torch.randn(1, ))
+        grad_mod2_cost = Variable(torch.randn(1, ))
+        # inp_mod3_cost = Variable(torch.randn(1, ))
+        ###
 
-        mod1_output_true_grad, mod1_w_true_grad = torch.autograd.grad(module2_output,
-                                                                      [module1_output] + list(module1.parameters()),
-                                                                      grad_outputs=mod2_output_true_grad,
-                                                                      retain_graph=True)
-        grad_mod1_cost = optimize_module(grad_module1_opt,
-                                         mod1_generated_grad,
-                                         mod1_w_true_grad)
-        # grad_mod1_cost = Variable(torch.randn(1,))
-        # grad_mod2_cost = Variable(torch.randn(1,))
+        # Training input module 3
+        inp_mod3_cost = inp_module_3.optimize_itself(inp_mod3_generated_sample, layer2_output)
 
-        if print_fn(step):
+        mod2_output_true_grad = torch.autograd.grad(mod3_cost,
+                                                    [inp_mod3_generated_sample],
+                                                    grad_outputs=None,
+                                                    retain_graph=True)[0]
+        grad_mod2_cost = grad_module2.optimize_itself(layer2_generated_grad, mod2_output_true_grad)
+
+        mod1_output_true_grad = torch.autograd.grad(layer2_output,
+                                                    [layer1_output],
+                                                    grad_outputs=layer2_generated_grad,
+                                                    retain_graph=True)[0]
+        grad_mod1_cost = grad_module1.optimize_itself(layer1_generated_grad, mod1_output_true_grad)
+
+        if step % 100 == 0:
             print("epoch", epoch, "   step", step)
             stp = len(mnist.train_loader.dataset) * epoch + step * batch_size
             plot_mod3_loss.update(stp, mod3_cost)
-            plot_grad_mod1_loss.update(stp, grad_mod1_cost)
-            plot_grad_mod2_loss.update(stp, grad_mod2_cost)
-            plot_mod1_gen_grad_norm.update(stp, mod1_generated_grad.norm())
-            plot_mod2_gen_grad_norm.update(stp, mod2_generated_grad.norm())
+            grad_module1.plot.update(stp, grad_mod1_cost)
+            grad_module2.plot.update(stp, grad_mod2_cost)
+            plot_mod1_gen_grad_norm.update(stp, layer1_generated_grad.norm())
+            plot_mod2_gen_grad_norm.update(stp, layer2_generated_grad.norm())
+            inp_module_3.plot.update(stp, inp_mod3_cost)
             try:
                 text = "grad_mod1_cost {:.2f}, " \
                        "grad_mod2_cost {:.2f}, " \
                        "mod3_cost {:.2f}, " \
                        "mod1_generated_grads norm {:.2f}, " \
-                       "mod2_generated_grads norm {:.2f} "
+                       "mod2_generated_grads norm {:.2f}, " \
+                       "inp_mod3_cost {:.2f} "
                 print(text.format(float(grad_mod1_cost.data.cpu().numpy()),
                                   float(grad_mod2_cost.data.cpu().numpy()),
                                   float(mod3_cost.data.cpu().numpy()),
-                                  float(mod1_generated_grad.norm().cpu().data.numpy()),
-                                  float(mod2_generated_grad.norm().cpu().data.numpy())))
+                                  float(layer1_generated_grad.norm().cpu().data.numpy()),
+                                  float(layer2_generated_grad.norm().cpu().data.numpy()),
+                                  float(inp_mod3_cost.data.cpu().numpy())
+                                  ))
             except NameError:
                 pass
             test_network()
--- src/modules.py
+++ src/modules.py
@@ -1,12 +1,90 @@
+import numbers
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
+from src.plot import *
 
 
 class Identity:
     def __call__(self, x):
         return x
+
+
+class Affine(nn.Module):
+    def __init__(self, input_size, output_size, nonlinearity=Identity):
+        super().__init__()
+        self.nonlinearity = nonlinearity()
+        self.w = nn.Parameter(torch.nn.init.xavier_normal(torch.randn(input_size + 1, output_size)))
+
+    def forward(self, x):
+        ones = Variable(torch.ones(x.data.shape[0], 1)).cuda()
+        out = torch.cat((x, ones), dim=1)
+        out = out @ self.w
+        out = self.nonlinearity(out)
+        return out
+
+    def set_grads(self, layer_output, input_grad):
+        grad = torch.autograd.grad(layer_output, self.w, grad_outputs=input_grad)[0]
+        self.w.grad = grad
+
+
+class Module(nn.Module):
+    def __init__(self, input_size,
+                 output_shape,
+                 nonlinearity=Identity,
+                 lr=0.001,
+                 zero_init=True,
+                 name="Gradient Module",
+                 reshape_and_sum=True):
+        super().__init__()
+        self.nonlinearity = nonlinearity()
+        self.input_size = input_size
+        self.output_shape = output_shape
+        self.output_size = int(np.prod(output_shape))
+        self.reshape_and_sum = reshape_and_sum
+
+        self.w = nn.Parameter(torch.nn.init.xavier_normal(torch.randn(input_size + 1, self.output_size), gain=10))
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+        self.plot = Plot(name)
+        if zero_init:
+            self.zero_init()
+
+    def zero_init(self):
+        for param in self.parameters():
+            param.data.zero_()
+
+    def forward(self, x):
+        ones = Variable(torch.ones(x.data.shape[0], 1)).cuda()
+        out = torch.cat((x, ones), dim=1)
+        out @= self.w
+        out = self.nonlinearity(out)
+
+        if self.reshape_and_sum:
+            out = out.view(-1, *self.output_shape)  # reshape to output_shape
+            out = out.sum(dim=0)  # sum batch
+        return out
+
+    def optimize_itself(self, module_generated_sample, module_true_sample):
+        self.optimizer.zero_grad()
+        if module_true_sample.volatile is True:
+            module_true_sample.volatile = False  # why do I have to set this?
+        module_cost = squared_loss_sum(module_true_sample, module_generated_sample)
+        module_cost.backward(retain_graph=True)
+        self.optimizer.step()
+        return module_cost
+
+
+class SquaredDifferenceLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, grads_true, grads_predicted):
+        out = torch.mean((grads_true - grads_predicted) ** 2)
+        return out
+
+
+squared_loss_sum = SquaredDifferenceLoss()
 
 
 class Net(nn.Module):
@@ -22,54 +100,3 @@ class Net(nn.Module):
             if i < len(self.layers) - 1:
                 x = self.nonlinearity(x)
         return x
-
-
-class OneLayer(nn.Module):
-    def __init__(self, input_size, output_size, nonlinearity=Identity):
-        super().__init__()
-        self.nonlinearity = nonlinearity()
-        self.w = nn.Parameter(torch.nn.init.xavier_normal(torch.randn(input_size + 1, output_size)))
-
-    def forward(self, x):
-        ones = Variable(torch.ones(x.data.shape[0], 1)).cuda()
-        out = torch.cat((x, ones), dim=1)
-        out = out @ self.w
-        out = self.nonlinearity(out)
-        return out
-
-    def set_grads(self, grad):
-        self.w.grad = grad
-
-
-class GradModule(nn.Module):
-    def __init__(self, input_size, output_shape, nonlinearity=Identity):
-        super().__init__()
-        self.nonlinearity = nonlinearity()
-        self.input_size = input_size
-        self.output_shape = output_shape
-        self.output_size = int(np.prod(output_shape))
-
-        self.w = nn.Parameter(torch.nn.init.xavier_normal(torch.randn(input_size + 1, self.output_size), gain=10))
-        self.zero_init()
-
-    def zero_init(self):
-        for param in self.parameters():
-            param.data.zero_()
-
-    def forward(self, x):
-        ones = Variable(torch.ones(x.data.shape[0], 1)).cuda()
-        out = torch.cat((x, ones), dim=1)
-        out @= self.w
-        out = self.nonlinearity(out)
-
-        out = out.view(-1, *[int(p) for p in self.output_shape]).sum(dim=0)  # reshape to output_shape and sum batch
-        return out
-
-
-class SquaredDifferenceLoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, grads_true, grads_predicted):
-        out = torch.mean((grads_true - grads_predicted) ** 2)
-        return out
